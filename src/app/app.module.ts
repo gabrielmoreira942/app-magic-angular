4999cd7439d85ed82eadd7db81fbbb7f26fbdc0d
gabriel <maboroshiken@gmail.com>
2024-05-14 13:27:06 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import SearchComponent from 'src/app/search/search.component';
-
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,28 @@
 
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import axios from 'axios';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export default class FormComponent {
+export default class FormComponent implements OnInit {
   formData: any = {};
 
+  ngOnInit(): void {
+    this.getCards()
+  }
   submitForm() {
-    console.log('Dados do formulário:', this.formData);
+    console.log(this.formData);
+  }
+  getCards() {
+    axios.get('https://api.magicthegathering.io/v1/cards')
+      .then(response => {
+        this.formData.blocks = response.data.cards
+      })
+      .catch(error => {
+        console.error('Erro:', error);
+      });
   }
 }
